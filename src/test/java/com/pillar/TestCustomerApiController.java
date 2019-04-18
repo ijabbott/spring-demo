d306e80a701c2e384e2be652fe13b9a6d7745446
Ian Abbott <ij.abbott2306@gmail.com>
2019-04-18 14:45:09 -0500

    Refactoring tests

--- src/test/java/com/pillar/TestCustomerApiController.java
+++ src/test/java/com/pillar/TestCustomerApiController.java
@@ -3,6 +3,7 @@ package com.pillar;
 import com.pillar.demo.Customer;
 import com.pillar.demo.CustomerApiController;
 import com.pillar.demo.CustomerRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,21 +12,24 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 public class TestCustomerApiController {
+    private CustomerApiController controller;
+
+    @Before
+    public void setup() {
+        CustomerRepository repo = mock(CustomerRepository.class);
+        controller = new CustomerApiController(repo);
+    }
 
     @Test
-    public void givenCreatingCustomerWhenGoodCustomerRequestThenReturnsOk(){
+    public void givenCreatingCustomerWhenGoodCustomerRequestThenReturnsOk() {
         Customer customer = new Customer("Awesome", "Customer");
-        CustomerRepository repo = mock(CustomerRepository.class);
-        CustomerApiController controller = new CustomerApiController(repo);
         ResponseEntity<Customer> response = controller.create(customer);
         HttpStatus statusCode = response.getStatusCode();
         assertEquals(HttpStatus.CREATED, statusCode);
     }
 
     @Test
-    public void healthEndpointReturnsOk(){
-        CustomerRepository repo = mock(CustomerRepository.class);
-        CustomerApiController controller = new CustomerApiController(repo);
+    public void healthEndpointReturnsOk() {
         ResponseEntity<String> response = controller.health();
         HttpStatus statusCode = response.getStatusCode();
         assertEquals(HttpStatus.OK, statusCode);
